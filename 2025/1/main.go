39f17b86be9451e0dd45121d4511b1825d133b13
TylerHelmuth <12352919+TylerHelmuth@users.noreply.github.com>
2025-11-30 22:52:31 -0700

    2025 day 1

--- 2025/1/main.go
+++ 2025/1/main.go
@@ -1,19 +1,76 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-//func part1(lines []string) float64 {
-//
-//}
-//
-//func part2(lines []string) float64 {
-//
-//}
+func part1(rotations []string) int64 {
+	numTimesHitZero := int64(0)
+	currentPosition := int64(50)
+	for _, rotation := range rotations {
+		num, _ := strconv.ParseInt(rotation[1:], 10, 64)
+		if rotation[0] == 'R' {
+			currentPosition += num
+		} else {
+			currentPosition -= num
+		}
+
+		if currentPosition < 0 {
+			for currentPosition < 0 {
+				currentPosition += 100
+			}
+		} else if currentPosition > 99 {
+			for currentPosition > 99 {
+				currentPosition -= 100
+			}
+		}
+
+		if currentPosition == 0 {
+			numTimesHitZero++
+		}
+	}
+	return numTimesHitZero
+}
+
+func part2(rotations []string) int64 {
+	numTimesHitZero := int64(0)
+	currentPosition := int64(50)
+	for _, rotation := range rotations {
+		startingPosition := currentPosition
+		num, _ := strconv.ParseInt(rotation[1:], 10, 64)
+		if rotation[0] == 'R' {
+			currentPosition += num
+		} else {
+			currentPosition -= num
+		}
+
+		if currentPosition < 0 {
+			if startingPosition == 0 {
+				numTimesHitZero--
+			}
+			for currentPosition < 0 {
+				currentPosition += 100
+				numTimesHitZero++
+			}
+			if currentPosition == 0 {
+				numTimesHitZero++
+			}
+		} else if currentPosition > 99 {
+			for currentPosition > 99 {
+				currentPosition -= 100
+				numTimesHitZero++
+			}
+		} else if currentPosition == 0 {
+			numTimesHitZero++
+		}
+	}
+	return numTimesHitZero
+}
 
 func main() {
 	f, err := os.Open("/Users/tylerhelmuth/Projects/advent-of-code/2025/1/input.txt")
@@ -24,13 +81,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	lines := strings.Split(string(b), "\n")
+	rotations := strings.Split(string(b), "\n")
 
-	grid := make([][]string, len(lines))
-	for i, l := range lines {
-		grid[i] = strings.Split(l, "")
-	}
-
-	//fmt.Println(fmt.Sprintf("part 1 count is %d", part1(grid)))
-	//fmt.Println(fmt.Sprintf("part 2 count is %d", part2(grid)))
+	fmt.Println(fmt.Sprintf("part 1 password is %d", part1(rotations)))
+	fmt.Println(fmt.Sprintf("part 2 password is %d", part2(rotations)))
 }
